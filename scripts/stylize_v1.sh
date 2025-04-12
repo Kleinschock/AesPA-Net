4a709d89fb31c788957a74b2f397b202552d7906
Kleinschock <49147218+Kleinschock@users.noreply.github.com>
2025-04-12 19:14:55 +0200

    Working stylize working for real

---
+++ scripts/stylize_v1.sh
@@ -0,0 +1,8 @@
+python ./main.py \
+    --type test \
+    --batch_size 1 \
+    --comment aepapa_run1 \
+    --content_dir ./content \
+    --style_dir ./style \
+    --num_workers 4 \
+    --test_result_dir ./test_results
